748bdb24977d401ff65ac5a8e969f6a9b4e0ba1e
navateja45 <navateja88011@gmail.com>
2023-10-12 11:20:04 +0530

    initial

---
+++ frontend/src/component/layout/ContactPage/ContactPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./ContactPage.css"
+
+const ContactPage = () => {
+  return (
+    <div>Contact : navateja88011@gmail.com</div>
+  )
+}
+
+export default ContactPage
